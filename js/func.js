ab8b059ef96914fe26ae12183edafad6cf6b1638
weicaiyue <weicaiyue528@163.com>
2017-11-03 19:08:07 +0800

    change style
    remove the text area.add a timeout function to better the transition effect

--- js/func.js
+++ js/func.js
@@ -1,6 +1,15 @@
-let vm=new Vue({
+var vm=new Vue({
 	el:'#app',
 	data:{
 		full:true,
+		remove:false,
+	},
+	methods:{
+		removeText:()=>{
+			vm.full=false;
+			setTimeout(()=>{
+				vm.remove=true;
+			},650);
+		}
 	}
 })
